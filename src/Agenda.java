60db0a83d5630f7e6d6d671aa97baa4de5053fe6
HandlezGZ <tirsoneto@hotmail.com>
2024-01-23 18:03:07 -0300

    metodo adicionarContato e gerarNovoIdContato implementados

--- src/Agenda.java
+++ src/Agenda.java
@@ -22,9 +22,44 @@ public class Agenda {
             System.out.println("2 - Remover Contato");
             System.out.println("3 - Editar Contato");
             System.out.println("4 - Sair");
+
             entrada = scanner.nextInt();
+            if (entrada == 1) {
+                adicionarContato();
+            }
 
         }
         scanner.close();
+    }
+
+    private static void adicionarContato() {
+        Scanner scanner = new Scanner(System.in);
+
+        Contato novoContato = new Contato();
+        novoContato.setId(gerarNovoIdContato());
+        editarNomeContato(novoContato);
+        editarSobrenomeContato(novoContato);
+    }
+
+    private static Long gerarNovoIdContato() {
+        if (contatos.size() == 0) {
+            return 1L;
+        }
+        Long ultimoContatoID = contatos.get(contatos.size()-1).getId();
+        return (ultimoContatoID+1);
+    }
+
+    private static void editarNomeContato(Contato contato) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("DDigite o nome do contato: ");
+        String nomeContato = scanner.next();
+        contato.setNome(nomeContato);
+    }
+
+    private static void editarSobrenomeContato(Contato contato) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o sobrenome do contato: ");
+        String sobrenomeContato = scanner.next();
+        contato.setSobrenome(sobrenomeContato);
     }
 }
